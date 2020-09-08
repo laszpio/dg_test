ac0f52b04e69319838cc8177d370ec4873984bc0
Piotr Laszewski <laszpio@gmail.com>
2020-09-08 15:51:28 +0200

    DgTest#post parse for authors.

--- lib/dg_test.ex
+++ lib/dg_test.ex
@@ -7,13 +7,36 @@ defmodule DgTest do
   def posts() do
     page = posts(1)
 
-    1..page_max(page)
+    1..1
     |> Enum.map(&posts/1)
+    |> Enum.map(&parse_posts/1)
+    |> Enum.to_list()
+    |> List.flatten()
   end
 
   def posts(page) do
     {:ok, resp} = get("/posts/", query: [key: ghost_key(), page: page, include: "authors,tags"])
     resp.body
+  end
+
+  def parse_posts(page) do
+    Map.get(page, "posts") |> Enum.map(&parse_post/1)
+  end
+
+  def parse_post(post) do
+    %{
+      title: Map.get(post, "title"),
+      tags: parse_tags(post),
+      authors: parse_authors(post)
+    }
+  end
+
+  def parse_authors(post) do
+    post |> Map.get("authors") |> Enum.map(&Map.get(&1, "name"))
+  end
+
+  def parse_tags(post) do
+
   end
 
   def ghost_url do
