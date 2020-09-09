3bce92ef287a5555484d1ba47cd40dfee6897ad1
Piotr Laszewski <laszpio@gmail.com>
2020-09-09 10:56:10 +0200

    Use: .env to configure local

--- config/dev.exs
+++ config/dev.exs
@@ -1,9 +1,9 @@
 import Config
 
 config :dg_test,
-  ghost_url: "https://productmarketingalliance.com/ghost/api/v3/content",
+  ghost_url: "#{System.get_env("GHOST_URL")}",
   ghost_key: "#{System.get_env("GHOST_KEY")}",
-  solr_url: "http://localhost:8983/solr",
-  solr_core: "posts"
+  solr_url: "#{System.get_env("SOLR_URL")}",
+  solr_core: "#{System.get_env("SOLR_CORE")}"
 
 config :logger, level: :info
