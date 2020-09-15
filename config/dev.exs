7b7afdcb64b5336d5c4988e914f0d02d536456b4
Piotr Laszewski <laszpio@gmail.com>
2020-09-15 16:22:03 +0200

    Configuration for DEV

--- config/dev.exs
+++ config/dev.exs
@@ -1,9 +1,9 @@
 import Config
 
 config :dg_test,
-  ghost_url: "#{System.get_env("GHOST_URL")}",
-  ghost_key: "#{System.get_env("GHOST_KEY")}",
-  solr_url: "#{System.get_env("SOLR_URL")}",
-  solr_core: "#{System.get_env("SOLR_CORE")}"
+  ghost_url: System.get_env("GHOST_URL"),
+  ghost_key: System.get_env("GHOST_KEY"),
+  solr_url: System.get_env("SOLR_URL"),
+  solr_core: System.get_env("SOLR_CORE")
 
 config :logger, level: :info
