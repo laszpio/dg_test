76f9e35fad2884e74e975849f093a5fc88eac57a
Piotr Laszewski <laszpio@gmail.com>
2020-09-21 20:06:57 +0200

    DgTest.Solr.Core structure.

---
+++ lib/dg_test/solr/core.ex
@@ -0,0 +1,20 @@
+defmodule DgTest.Solr.Core do
+  defstruct [
+    :name,
+    :schema,
+    :start_time,
+    :uptime,
+    :index,
+    :dataDir
+  ]
+
+  def to_struct(kind, attrs) do
+    struct = struct(kind)
+    Enum.reduce Map.to_list(struct), struct, fn {k, _}, acc ->
+      case Map.fetch(attrs, Atom.to_string(k)) do
+        {:ok, v} -> %{acc | k => v}
+        :error -> acc
+      end
+    end
+  end
+end
