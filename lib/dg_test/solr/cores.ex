b0e1319d34472d460e748c34f121a154a14b5ad5
Piotr Laszewski <laszpio@gmail.com>
2020-09-21 08:26:47 +0200

    Solr.Cores: status/0 WIP

--- lib/dg_test/solr/cores.ex
+++ lib/dg_test/solr/cores.ex
@@ -1,5 +1,13 @@
-defmodule Cores do
+defmodule DgTest.Solr.Cores do
+  use Tesla
+
+  @base_url "#{DgTest.solr_url()}/admin/cores"
+
+  plug Tesla.Middleware.JSON
+  plug Tesla.Middleware.Logger, log_level: :info
+
   def status do
+    get(@base_url, action: "STATUS")
   end
 
   def status(name) do
