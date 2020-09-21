4691280d317ed27570a43f413bd521c3682aeebe
Piotr Laszewski <laszpio@gmail.com>
2020-09-21 10:49:09 +0200

    DgTest config: set up solr_url and solr_core via ENV.

--- config/test.exs
+++ config/test.exs
@@ -2,7 +2,8 @@ import Config
 
 config :dg_test,
   ghost_url: "https://ghost.local/api/v3/content",
-  ghost_key: "secret"
+  ghost_key: "secret",
+  solr_url: System.get_env("SOLR_URL"),
+  solr_core: System.get_env("SOLR_CORE")
 
 config :logger, level: :info
-
