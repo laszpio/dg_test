636463f3f2f1470f37b27006e5703421e966a7c3
Piotr Laszewski <laszpio@gmail.com>
2020-09-21 08:33:51 +0200

    Solr.Cores: set up admin path in Tesla.Middleware.BaseUrl

--- lib/dg_test/solr/cores.ex
+++ lib/dg_test/solr/cores.ex
@@ -1,13 +1,12 @@
 defmodule DgTest.Solr.Cores do
   use Tesla
 
-  @base_url "#{DgTest.solr_url()}/admin/cores"
-
+  plug Tesla.Middleware.BaseUrl, "#{DgTest.solr_url()}/admin"
   plug Tesla.Middleware.JSON
   plug Tesla.Middleware.Logger, log_level: :info
 
   def status do
-    get(@base_url, action: "STATUS")
+    get("/cores", action: "STATUS")
   end
 
   def status(name) do
