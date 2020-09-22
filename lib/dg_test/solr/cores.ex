cea46ee5148534794bcc80659350d851396a8012
Piotr Laszewski <laszpio@gmail.com>
2020-09-22 09:03:39 +0200

    Solr.Cores: uses Tesla.

--- lib/dg_test/solr/cores.ex
+++ lib/dg_test/solr/cores.ex
@@ -1,15 +1,17 @@
 defmodule DgTest.Solr.Cores do
+  use Tesla, only: [:get]
+
   alias DgTest.Solr.AdminApi
 
-  def status() do
-    case Tesla.get(AdminApi.client(), "/cores", query: [action: "STATUS"]) do
+  def status do
+    case get(AdminApi.client(), "/cores", query: [action: "STATUS"]) do
       {:ok, %Tesla.Env{status: 200, body: body}} -> {:ok, body}
       {:error, msg} -> {:error, msg}
     end
   end
 
   def status(core) do
-    case Tesla.get(AdminApi.client(), "/cores", query: [action: "STATUS", core: core]) do
+    case get(AdminApi.client(), "/cores", query: [action: "STATUS", core: core]) do
       {:ok, %Tesla.Env{status: 200, body: body}} -> {:ok, body}
       {:error, msg} -> {:error, msg}
     end
@@ -33,6 +35,13 @@ defmodule DgTest.Solr.Cores do
     case System.cmd("solr", ["delete", "-c", core]) do
       {_, 0} -> {:ok, "Deleted core '#{core}'"}
       {_, 1} -> {:error, "Failed to delete core '#{core}'"}
+    end
+  end
+
+  def rename(core, other) do
+    case get(AdminApi.client(), "/cores", query: [action: "RENAME", core: core, other: other]) do
+      {:ok, %Tesla.Env{status: 200, body: body}} -> {:ok, body}
+      {:error, msg} -> {:error, msg}
     end
   end
 end
