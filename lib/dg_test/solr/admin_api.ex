3bb102e72bfff832aa1b3f8ee87ff4d0cf444803
Piotr Laszewski <laszpio@gmail.com>
2020-09-22 09:04:57 +0200

    Solr.AdminApi: log level at :info.

--- lib/dg_test/solr/admin_api.ex
+++ lib/dg_test/solr/admin_api.ex
@@ -6,7 +6,8 @@ defmodule DgTest.Solr.AdminApi do
   def middleware do
     [
       {Tesla.Middleware.BaseUrl, "#{DgTest.solr_url()}/admin"},
-      Tesla.Middleware.JSON
+      Tesla.Middleware.JSON,
+      {Tesla.Middleware.Logger, log_level: :info}
     ]
   end
 end
