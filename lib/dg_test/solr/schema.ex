c0334f0ddff90ede961dc890f2c450fafdfab5cf
Piotr Laszewski <laszpio@gmail.com>
2020-09-23 09:21:25 +0200

    Solr.Schema: add_field/1 skeleton

--- lib/dg_test/solr/schema.ex
+++ lib/dg_test/solr/schema.ex
@@ -1,10 +1,19 @@
 defmodule DgTest.Solr.Schema do
-  use Tesla, only: [:get]
+  use Tesla, only: [:get, :post]
 
   def info(core) do
-    case get!(client(), "/#{core}/schema/") do
+    case get!(client(), "/solr/#{core}/schema/") do
       %Tesla.Env{status: 200, body: body} -> Jason.decode(body)
       %Tesla.Env{status: 404} -> {:error, "Core '#{core}' doesn't exist."}
+    end
+  end
+
+  def add_field(core, name) do
+    path = "/api/collections/#{core}/schema"
+    data = %{"add-field" => %{name: "created_at", type: "pdate", stored: true}}
+
+    case post!(client(), path, data) do
+      %Tesla.Env{status: 200, body: body} -> body
     end
   end
 
