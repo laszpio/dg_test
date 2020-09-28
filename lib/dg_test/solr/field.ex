aa2d45411c11771044660c71eaf8a23ef82d0718
Piotr Laszewski <laszpio@gmail.com>
2020-09-28 15:36:11 +0200

    Solr.Field: add multivalued

--- lib/dg_test/solr/field.ex
+++ lib/dg_test/solr/field.ex
@@ -2,13 +2,14 @@ defmodule DgTest.Solr.Field do
   alias DgTest.Solr.Utils
 
   @type t :: %__MODULE__{
-    name: binary,
-    type: binary,
-    indexed: binary,
-    required: boolean,
-    stored: boolean,
-    uninvertible: boolean
-  }
+          name: binary,
+          type: binary,
+          indexed: binary,
+          required: boolean,
+          stored: boolean,
+          uninvertible: boolean,
+          multivalued: boolean
+        }
 
   defstruct [
     :name,
@@ -16,10 +17,12 @@ defmodule DgTest.Solr.Field do
     :required,
     :stored,
     :type,
-    :uninvertible
+    :uninvertible,
+    :multivalued
   ]
 
   def new(field) do
     Utils.to_struct(%__MODULE__{}, field)
+    |> Map.put(:multivalued, field["multiValued"])
   end
 end
