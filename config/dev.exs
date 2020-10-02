a63c7cd3d2161c48f36a930c8572a47ca70ae73a
Piotr Laszewski <laszpio@gmail.com>
2020-10-02 09:42:47 +0200

    Config: dev/test add SOLR_CMD from ENV

--- config/dev.exs
+++ config/dev.exs
@@ -4,6 +4,7 @@ config :dg_test,
   ghost_url: System.get_env("GHOST_URL"),
   ghost_key: System.get_env("GHOST_KEY"),
   solr_url: System.get_env("SOLR_URL"),
-  solr_core: System.get_env("SOLR_CORE")
+  solr_core: System.get_env("SOLR_CORE"),
+  solr_cmd: System.get_env("SOLR_CMD")
 
 config :logger, level: :info
--- config/test.exs
+++ config/test.exs
@@ -4,7 +4,8 @@ config :dg_test,
   ghost_url: "https://ghost.local/api/v3/content",
   ghost_key: "secret",
   solr_url: System.get_env("SOLR_URL"),
-  solr_core: System.get_env("SOLR_CORE")
+  solr_core: System.get_env("SOLR_CORE"),
+  solr_cmd: System.get_env("SOLR_CMD")
 
 config :logger, level: :warn
 
