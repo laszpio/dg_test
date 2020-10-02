3af757a0614d657d39b185901181451f02e60d9f
Piotr Laszewski <laszpio@gmail.com>
2020-10-02 13:24:47 +0200

    Solr.AdminCmdTest: tests for solr_cmd/0 run/1 with general ‘solr_cmd’ configuration mock.

--- test/solr/admin_cmd_test.exs
+++ test/solr/admin_cmd_test.exs
@@ -4,25 +4,37 @@ defmodule DgTest.Solr.AdminCmdTest do
   import Mock
   import DgTest.Solr.AdminCmd
 
-  describe "run/1" do
-    test "run/1 with empty command" do
-      assert run("") == :error
-    end
+  setup_with_mocks([
+    {Application, [], [fetch_env!: fn :dg_test, :solr_cmd -> "runner args solr" end]}
+  ]) do
+    :ok
+  end
 
-    test "run/1 create" do
-    end
-
-    test "run/1 delete" do
+  describe "solr_cmd/0" do
+    test "solr_cmd/0 returns command string" do
+      assert solr_cmd() == "runner args solr"
+      assert called(Application.fetch_env!(:dg_test, :solr_cmd))
     end
   end
 
+  describe "run/1" do
+    test "create" do
+      with_mock System,
+        cmd: fn "runner", ["args", "solr", "create", "-c", "test"], stderr_to_stdout: true ->
+          {"output", 0}
+        end do
+        assert run("create -c test") == {:ok, "output"}
+        # assert called(System.cmd("runner", ["args", "solr", "create", "-c", "test"]))
+      end
+    end
 
-  describe "solr_cmd" do
-    test "solr_cmd/0 return command string" do
-      with_mock Application,
-        fetch_env!: fn :dg_test, :solr_cmd -> "runner args solr" end do
-        assert solr_cmd() == "runner args solr"
-        assert called(Application.fetch_env!(:dg_test, :solr_cmd))
+    test "delete" do
+      with_mock System,
+        cmd: fn "runner", ["args", "solr", "delete", "-c", "test"], stderr_to_stdout: true ->
+          {"output", 0}
+        end do
+        assert run("delete -c test") == {:ok, "output"}
+        # assert called(System.cmd("runner", ["args", "solr", "delete", "-c", "test"]))
       end
     end
   end
