4877d8abcf2c140e7e88076d029b094726a07ead
Piotr Laszewski <laszpio@gmail.com>
2020-10-02 08:22:22 +0200

    Solr.AdminCmd: solr_cmd/1

--- lib/dg_test/solr/admin_cmd.ex
+++ lib/dg_test/solr/admin_cmd.ex
@@ -1,18 +1,9 @@
 defmodule DgTest.Solr.AdminCmd do
   @moduledoc false
 
-  def exec(cmd) do
-  end
+  def solr_cmd(cmd) do
+    [base | opts] = System.get_env("SOLR_CMD") |> String.split()
 
-  def solr_cmd_base do
-    [cmd_base, _] = solr_cmd |> String.split()
-  end
-
-  def solr_cmd_opts do
-    [_, cmd_opts] = solr_cmd |> String.split()
-  end
-
-  def solr_cmd do
-    DgTest.solr_cmd()
+    {base, opts}
   end
 end
