e9cdc9ee2ac4d1ade5a46b1089ea14b3b07785f5
Piotr Laszewski <laszpio@gmail.com>
2020-10-02 12:48:51 +0200

    SolrAdminCmd: run/1 test for command with runner

--- test/solr/admin_cmd_test.exs
+++ test/solr/admin_cmd_test.exs
@@ -4,11 +4,25 @@ defmodule DgTest.Solr.AdminCmdTest do
   import Mock
   import DgTest.Solr.AdminCmd
 
+  describe "run/1" do
+    test "run/1 with empty command" do
+      assert run("") == :error
+    end
+
+    test "run/1 create" do
+    end
+
+    test "run/1 delete" do
+    end
+  end
+
+
   describe "solr_cmd" do
     test "solr_cmd/0 return command string" do
       with_mock Application,
-        fetch_env!: fn :dg_test, :solr_cmd -> "solr args" end do
-        assert solr_cmd() == "solr args"
+        fetch_env!: fn :dg_test, :solr_cmd -> "runner args solr" end do
+        assert solr_cmd() == "runner args solr"
+        assert called(Application.fetch_env!(:dg_test, :solr_cmd))
       end
     end
   end
