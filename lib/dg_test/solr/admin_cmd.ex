8efa0af95e1357d0b74891a15fafeff2ca364713
Piotr Laszewski <laszpio@gmail.com>
2020-10-02 09:43:35 +0200

    Solr.AdminCMD: solr_cmd/0 returns solr’s command path

--- lib/dg_test/solr/admin_cmd.ex
+++ lib/dg_test/solr/admin_cmd.ex
@@ -4,11 +4,15 @@ defmodule DgTest.Solr.AdminCmd do
   @cmd_opts [stderr_to_stdout: true]
 
   def run(cmd) do
-    [base | opts] = System.get_env("SOLR_CMD") |> String.split()
+    [base | opts] = solr_cmd() |> String.split()
 
     case Kernel.apply(System, :cmd, [base, opts ++ String.split(cmd), @cmd_opts]) do
       {output, 0} -> {:ok, output}
       {output, 1} -> {:error, output}
     end
+  end
+
+  def solr_cmd do
+    Application.fetch_env!(:dg_test, :solr_cmd)
   end
 end
