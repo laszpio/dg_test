28093c45c3699d0dd40981f5a8c6688aff244f0b
Piotr Laszewski <laszpio@gmail.com>
2020-10-05 09:23:38 +0200

    Ghost.Post: new/1 with defaults and collections

--- lib/dg_test/ghost/post.ex
+++ lib/dg_test/ghost/post.ex
@@ -3,60 +3,33 @@ defmodule DgTest.Ghost.Post do
 
   import HtmlSanitizeEx
 
+  @mapping [content: :html]
+
   defstruct [
     :id,
-    :domain,
     :slug,
     :title,
     :content,
     :created_at,
+    domain: "https://productmarketingalliance.com",
     tags: [],
     authors: [],
   ]
 
   def new(post) do
-    %__MODULE__{}
-    |> Map.put(:id, parse_id(post))
-    |> Map.put(:domain, "https://productmarketingalliance.com")
-    |> Map.put(:slug, parse_slug(post))
-    |> Map.put(:title, parse_title(post))
-    |> Map.put(:content, parse_content(post))
-    |> Map.put(:created_at, parse_created_at(post))
-    |> Map.put(:tags, parse_tags(post))
-    |> Map.put(:authors, parse_authors(post))
+    Enum.reduce(Map.to_list(%__MODULE__{}), %__MODULE__{}, fn {k, default}, acc ->
+      case Map.fetch(post, (Keyword.get(@mapping, k) || k) |> Atom.to_string) do
+        {:ok, v} -> %{acc | k => parse_attr(v, default)}
+        :error -> acc
+      end
+    end)
   end
 
-  def parse_id(post) do
-    post |> Map.get("id") |> strip_tags()
+  def parse_attr(value, []) do
+    value |> Enum.map(&Map.get(&1, "name")) |> Enum.map(&strip_tags/1)
   end
 
-  def parse_slug(post) do
-    post |> Map.get("slug") |> strip_tags()
-  end
-
-  def parse_title(post) do
-    post |> Map.get("title") |> strip_tags()
-  end
-
-  def parse_authors(post) do
-    post
-    |> Map.get("authors")
-    |> Enum.map(&Map.get(&1, "name"))
-    |> Enum.map(&strip_tags/1)
-  end
-
-  def parse_tags(post) do
-    post
-    |> Map.get("tags")
-    |> Enum.map(&Map.get(&1, "name"))
-    |> Enum.map(&strip_tags/1)
-  end
-
-  def parse_content(post) do
-    post |> Map.get("html") |> strip_tags()
-  end
-
-  def parse_created_at(post) do
-    post |> Map.get("created_at") |> strip_tags()
+  def parse_attr(value, _default) do
+    value |> strip_tags()
   end
 end
