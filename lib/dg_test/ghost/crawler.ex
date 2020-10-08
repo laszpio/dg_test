7c9f1ceca20cf4400e4e85d418bc03c7ac512389
Piotr Laszewski <laszpio@gmail.com>
2020-10-08 20:10:11 +0200

    Ghost.Crawler: new/1

--- lib/dg_test/ghost/crawler.ex
+++ lib/dg_test/ghost/crawler.ex
@@ -6,13 +6,15 @@ defmodule DgTest.Ghost.Crawler do
   import DgTest.Ghost
   alias DgTest.Ghost.Resource
 
-  @type t :: %__MODULE__{
-          domain: binary
-        }
+  @type t :: %__MODULE__{domain: binary}
 
   defstruct [:domain, :url, :key]
 
-  @resources ~w(posts pages)
+  @resources ~w(posts)
+
+  def new(domain) do
+    %Crawler{domain: domain}
+  end
 
   def resources(%Crawler{domain: domain} = _crawler) do
     Enum.map(@resources, &%Resource{domain: domain, name: &1, client: client()})
