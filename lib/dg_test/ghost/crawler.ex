841990ddaa393f2f11d63924c64ce3c9c83bf68f
Piotr Laszewski <laszpio@gmail.com>
2020-10-08 15:16:24 +0200

    Ghost.Crawler: resource/1

--- lib/dg_test/ghost/crawler.ex
+++ lib/dg_test/ghost/crawler.ex
@@ -1,8 +1,17 @@
 defmodule DgTest.Ghost.Crawler do
+  alias __MODULE__
+
+  alias DgTest.Ghost.Resource
+
   @type t :: %__MODULE__{
+          domain: binary,
           url: binary,
           key: binary
         }
 
-  defstruct [:url, :key]
+  defstruct [:domain, :url, :key]
+
+  def resources(%Crawler{domain: domain} = crawler) do
+    [%Resource{domain: domain, name: "posts"}]
+  end
 end
