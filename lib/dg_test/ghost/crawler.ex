becb69af4f801ac957ac3bc4fd70b10d2f37471f
Piotr Laszewski <laszpio@gmail.com>
2020-10-12 10:14:13 +0200

    Crawler: typespecs.

--- lib/dg_test/ghost/crawler.ex
+++ lib/dg_test/ghost/crawler.ex
@@ -11,26 +11,31 @@ defmodule DgTest.Ghost.Crawler do
 
   @resources ~w(posts)
 
+  @spec start(t) :: t
   def start(%Crawler{} = crawler) do
     case Client.start_link() do
       {:ok, pid} -> %{crawler | pid: pid}
     end
   end
 
+  @spec stop(t) :: t
   def stop(%Crawler{pid: pid} = crawler) do
     case Client.stop(pid) do
       :ok -> %{crawler | pid: nil}
     end
   end
 
+  @spec resources(t) :: t
   def resources(%Crawler{domain: domain} = crawler) do
     Map.put(crawler, :resources, Enum.map(@resources, &%Resource{domain: domain, name: &1}))
   end
 
+  @spec fetch(t) :: t
   def fetch(%Crawler{resources: resources} = crawler) do
     %{crawler | items: Enum.flat_map(resources, &Resource.all/1)}
   end
 
+  @spec items(t) :: list(map)
   def items(%Crawler{items: items} = _crawler) do
     Enum.map(items, &Map.from_struct/1)
   end
