08cba542b20090078357b3d5a457f20fa2242a50
Piotr Laszewski <laszpio@gmail.com>
2020-10-12 12:00:56 +0200

    Solr: start Solr.AdminApi genserver

--- lib/dg_test/solr.ex
+++ lib/dg_test/solr.ex
@@ -2,7 +2,10 @@ defmodule DgTest.Solr do
   use Application
 
   def start(_type, _args) do
-    children = []
+    children = [
+      {DgTest.Solr.SchemaApi, []}
+    ]
+
     Supervisor.start_link(children, strategy: :one_for_one)
   end
 
