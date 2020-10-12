39e45ca3756ebb0ef7682991f9496f949c1541ee
Piotr Laszewski <laszpio@gmail.com>
2020-10-12 10:14:08 +0200

    Ghost.Client: test for ghost_url/0, ghost_key/0

--- test/ghost/client_test.exs
+++ test/ghost/client_test.exs
@@ -4,13 +4,14 @@ defmodule DgTest.Ghost.ClientTest do
   import Mock
   import DgTest.Ghost.Client
 
-  @sample_api_url "http://localhost/ghost/api/v3/content"
+  @api_url "http://localhost/ghost/api/v3/content"
+  @api_key "token"
 
   describe "ghost_url/0" do
     test "returns API url" do
       with_mock Application,
-        fetch_env!: fn :dg_test, :ghost_url -> @sample_api_url end do
-        assert ghost_url() == @sample_api_url
+        fetch_env!: fn :dg_test, :ghost_url -> @api_url end do
+        assert ghost_url() == @api_url
         assert called(Application.fetch_env!(:dg_test, :ghost_url))
       end
     end
@@ -18,6 +19,11 @@ defmodule DgTest.Ghost.ClientTest do
 
   describe "ghost_key/0" do
     test "returs API key" do
+      with_mock Application,
+        fetch_env!: fn :dg_test, :ghost_key -> @api_key end do
+        assert ghost_key() == @api_key
+        assert called(Application.fetch_env!(:dg_test, :ghost_key))
+      end
     end
   end
 end
