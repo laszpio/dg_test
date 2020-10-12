dc6765ca2f2b96850b7f531b00a8824545f519c9
Piotr Laszewski <laszpio@gmail.com>
2020-10-12 10:14:08 +0200

    Client: basic genserver and get!/3

--- lib/dg_test/ghost/client.ex
+++ lib/dg_test/ghost/client.ex
@@ -1,7 +1,21 @@
 defmodule DgTest.Ghost.Client do
-  def get!(client, path, query: query) do
+  use GenServer
+
+  def init(client) do
+    {:ok, client}
+  end
+
+  def start_link(client) do
+    GenServer.start_link(__MODULE__, client, name: __MODULE__)
+  end
+
+  def get!(path, query: query) do
+    GenServer.call(__MODULE__, {:get!, path, query: query})
+  end
+
+  def handle_call({:get!, path, query: query}, _from, client) do
     case Tesla.get!(client, path, query: query) do
-      %Tesla.Env{status: 200, body: body} -> body
+      %Tesla.Env{status: 200, body: body} -> {:reply, body, client}
     end
   end
 end
