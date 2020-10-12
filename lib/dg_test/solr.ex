b1a079c4e91ebe47dfa3a7240c003fa38e987781
Piotr Laszewski <laszpio@gmail.com>
2020-10-12 10:50:59 +0200

    Solr: empty supervisor skeleton

--- lib/dg_test/solr.ex
+++ lib/dg_test/solr.ex
@@ -1,5 +1,10 @@
 defmodule DgTest.Solr do
-  @moduledoc false
+  use Application
+
+  def start(_type, _args) do
+    children = []
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
 
   @spec solr_url() :: binary
   def solr_url do
