bdaca1213fc1d7207ff0d145849fada02a9ed638
Piotr Laszewski <laszpio@gmail.com>
2020-10-13 10:56:56 +0200

    Solr.Client: skeleton

---
+++ lib/dg_test/solr/client.ex
@@ -0,0 +1,31 @@
+defmodule DgTest.Solr.Client do
+  use GenServer
+
+  alias DgTest.Solr
+
+  def init(state) do
+    {:ok, state}
+  end
+
+  def start_link(state \\ nil) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
+
+  def stop(pid) do
+    GenServer.stop(pid, :normal)
+  end
+
+  @spec client() :: %Tesla.Client{}
+  def client() do
+    Tesla.client(middleware())
+  end
+
+  @spec middleware() :: list(tuple)
+  def middleware() do
+    [
+      {Tesla.Middleware.BaseUrl, Solr.solr_url()},
+      {Tesla.Middleware.JSON, decode_content_types: ["text/plain"]},
+      {Tesla.Middleware.Logger, log_level: :info}
+    ]
+  end
+end
