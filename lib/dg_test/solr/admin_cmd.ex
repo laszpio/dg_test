471e288bc0a310616a403e31dc43699e71919f55
Piotr Laszewski <laszpio@gmail.com>
2020-10-13 20:27:43 +0200

    Solr.AdminCmd: run/1

--- lib/dg_test/solr/admin_cmd.ex
+++ lib/dg_test/solr/admin_cmd.ex
@@ -1,6 +1,8 @@
 defmodule DgTest.Solr.AdminCmd do
   use GenServer
 
+  @cmd_opts [stderr_to_stdout: true]
+
   def init(state) do
     {:ok, state}
   end
@@ -9,10 +11,16 @@ defmodule DgTest.Solr.AdminCmd do
     GenServer.start_link(__MODULE__, state, name: __MODULE__)
   end
 
-  @cmd_opts [stderr_to_stdout: true]
-
-  @spec run(binary) :: {:ok, binary} | {:error, binary}
   def run(cmd) do
+    GenServer.call(__MODULE__, {:run, cmd})
+  end
+
+  def handle_call({:run, cmd}, _from, state) do
+    {:reply, do_run(cmd), state}
+  end
+
+  @spec do_run(binary) :: {:ok, binary} | {:error, binary}
+  def do_run(cmd) do
     [base | opts] = solr_cmd() |> String.split()
 
     case Kernel.apply(System, :cmd, [base, opts ++ String.split(cmd), @cmd_opts]) do
