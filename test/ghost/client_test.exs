2ffc360ef6118351cbdd0aae127bd00320d8d4ab
Piotr Laszewski <laszpio@gmail.com>
2020-10-21 14:33:25 +0200

    Ghost.Client: check if returns Tesla.Client

--- test/ghost/client_test.exs
+++ test/ghost/client_test.exs
@@ -1,29 +1,16 @@
 defmodule DgTest.Ghost.ClientTest do
   use ExUnit.Case, async: true
 
-  import Mock
-  import DgTest.Ghost.Client
+  alias DgTest.Ghost.Client
 
   @api_url "http://localhost/ghost/api/v3/content"
   @api_key "token"
 
-  describe "ghost_api/0" do
-    test "returns API url" do
-      with_mock Application,
-        fetch_env!: fn :dg_test, :ghost_api -> @api_url end do
-        assert ghost_api() == @api_url
-        assert called(Application.fetch_env!(:dg_test, :ghost_api))
-      end
-    end
-  end
+  describe "client" do
+    test "returns Tesla client" do
+      client = Client.client(@api_url, @api_key)
 
-  describe "ghost_key/0" do
-    test "returs API key" do
-      with_mock Application,
-        fetch_env!: fn :dg_test, :ghost_key -> @api_key end do
-        assert ghost_key() == @api_key
-        assert called(Application.fetch_env!(:dg_test, :ghost_key))
-      end
+      assert %Tesla.client{} = client
     end
   end
 end
