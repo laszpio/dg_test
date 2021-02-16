512f148dedcd0c26e90cb3d4bc7c20f6ea5c21c7
Piotr Laszewski <laszpio@gmail.com>
2021-02-16 10:03:50 +0000

    Configure timeout for Solr

--- config/test.exs
+++ config/test.exs
@@ -5,7 +5,8 @@ config :dg_test,
   ghost_key: "secret",
   solr_url: System.get_env("SOLR_URL"),
   solr_core: System.get_env("SOLR_CORE"),
-  solr_cmd: System.get_env("SOLR_CMD")
+  solr_cmd: System.get_env("SOLR_CMD"),
+  solr_timeout: System.get_env("SOLR_TIMEOUT")
 
 config :logger, level: :warn
 
